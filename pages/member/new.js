c0be836353a036db82689083a0ebff870d521e58
Eric Frey <ericlfrey@gmail.com>
2023-01-21 13:56:21 -0600

    Add Member form is functional

--- pages/member/new.js
+++ pages/member/new.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import MemberForm from '../../components/MemberForm';
 
 export default function AddMemberPage() {
   return (
-    <h1>Add a Member</h1>
+    <>
+      <h1>Add a Member</h1>
+      <MemberForm />
+    </>
   );
 }
