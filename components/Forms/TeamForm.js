8fddcc72db4bfe3888409f332aad5432d7f70023
Eric Frey <ericlfrey@gmail.com>
2023-01-24 19:30:05 -0600

    Create Team form is functional. Need to make a team Card

--- components/Forms/TeamForm.js
+++ components/Forms/TeamForm.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { FloatingLabel, Form } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { createTeam, updateTeam } from '../../api/teams';
+import { useAuth } from '../../utils/context/authContext';
+
+const initialState = {
+  name: '',
+  public: false,
+};
 
 export default function TeamForm() {
+  const [formInput, setFormInput] = useState(initialState);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = { ...formInput, uid: user.uid };
+    createTeam(payload).then(({ name }) => {
+      const patchPayload = { firebaseKey: name };
+      updateTeam(patchPayload).then(() => {
+        router.push('/teams');
+      });
+    });
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">Add Team</h2>
 
       {/* NAME INPUT  */}
@@ -12,8 +41,8 @@ export default function TeamForm() {
           type="text"
           placeholder="Name"
           name="name"
-          // value={formInput.name}
-          // onChange={handleChange}
+          value={formInput.name}
+          onChange={handleChange}
           required
         />
       </FloatingLabel>
@@ -24,14 +53,15 @@ export default function TeamForm() {
         id="public"
         name="public"
         label="Public or Private"
-      // checked={formInput.sale}
-      // onChange={(e) => {
-      //   setFormInput((prevState) => ({
-      //     ...prevState,
-      //     sale: e.target.checked,
-      //   }));
-      // }}
+        checked={formInput.public}
+        onChange={(e) => {
+          setFormInput((prevState) => ({
+            ...prevState,
+            public: e.target.checked,
+          }));
+        }}
       />
+      <Button type="submit">Create Book</Button>
     </Form>
   );
 }
